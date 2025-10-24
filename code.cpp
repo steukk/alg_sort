6b92c9d763263666f0e9de2d44b9fd16b3377bab
steukk <lerastoik@gmail.com>
2025-10-24 09:20:02 +0300

    code.cpp

---
+++ code.cpp
@@ -0,0 +1,33 @@
+//Сортировка выбором
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
+    	cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+void selectionSort(int arr[], int n) {
+
+	for (int i = 0; i < n - 1; ++i) { 	
+    	int minIndex = i;
+    	for (int j = i + 1; j < n; ++j) {
+        	if (arr[j] < arr[minIndex]) {   
+            	minIndex = j;           	
+        	}
+    	}
+    	swap(arr[i], arr[minIndex]);   
+	}
+}
+int main() {
+	int arr[] = {64, 25, 12, 22, 11};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	cout << "Исходный массив:\n";
+	printArray(arr, n);
+	selectionSort(arr, n);             
+	cout << "\nОтсортированный массив:\n";
+	printArray(arr, n);
+
+	return 0;
+}
