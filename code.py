c4c34e85373e113d27541f8d50c05efb6625d347
steukk <lerastoik@gmail.com>
2025-10-24 09:24:21 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,13 @@
+# Функция сортировки пузырьком
+def bubble_sort(arr): 
+    n = len(arr)      
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+arr = [64, 34, 25, 12, 22, 11, 90]  
+print("Исходный массив:", arr)
+bubble_sort(arr)  
+print("Отсортированный массив:", arr)
 
